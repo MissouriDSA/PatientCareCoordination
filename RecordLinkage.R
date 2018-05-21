f5f35e23a7274defaa195f3e71ca6a2c74c0edbd
Sean P. Goggins <outdoors@acm.org>
2018-05-21 14:41:18 -0500

    chose a record linkage function

--- RecordLinkage.R
+++ RecordLinkage.R
@@ -1,5 +1,7 @@
+setwd("~/msgcenter")
+
 library(RecordLinkage)
 
-test <- read.csv("msg-text-only.csv")
+test <- read.csv("msg-center1.csv")
 
-  
+trythis <- compare.dedup (test, strcmp = TRUE, strcmpfun = levenshteinSim)
